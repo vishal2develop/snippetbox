a248d73b08de56ab6e331c99b51544e6b2d92b12
Vishal <vishal_borana@neimanmarcus.com>
2025-10-28 01:09:19 +0530

    feat: Static Files & UI Design - Added Static Files - Updated base templates to use the static files - Disabled directory traversal via browser, by creating empty index.html files at required dirs - Served Static files using http.FileServer

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,22 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Register static files
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Note that the path given to the http.Dir function is relative to the project
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Registers a handler for any URL path that starts with /static/.
+	/**
+	Why Strip the Prefix?
+	Without http.StripPrefix, there would be a mismatch:
+	Incoming request: GET /static/css/style.css
+	FileServer looks for: ./ui/static/static/css/style.css ❌ (looks for "static" twice!)
+	*/
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
 	// Register GET routes
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
