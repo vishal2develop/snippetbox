f539fb1a130072c69332873d54bf3ae9d2b61bec
Vishal <vishal_borana@neimanmarcus.com>
2025-10-28 14:15:47 +0530

    feat: Custom Logger - Created custom logger using log/slog - logger type - TextHandler logger: log o/p = text

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	// accept a new command line flag for the port
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// parse the flags and assign it to addr.
+	// Parse() must be called after all flags are defined and before flags are accessed.
+	// if not called, the flag will be set to the default value.
+	// any errors during parsing, the application will be terminated.
+	flag.Parse()
+
+	// Use the slog.New() function to initialize a new structured logger, which
+	// writes to the standard out stream and uses the default settings.
+	// second argument is a pointer to a slog.HandlerOptions struct , which you can use to customize the behavior of the handler. if happy, with default settings -> pass nil
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	mux := http.NewServeMux()
 
 	// Register static files
@@ -30,9 +47,13 @@ func main() {
 	// Register POST routes
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Println("Starting server on :4000")
+	// Value returned by flag.String() is a pointer to the flag's value and not the value itself.
+	// Hence, we need to dereference the pointer (prefix with *) to get the actual value.
+	logger.Info("Starting server on", "addr", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 
-	log.Fatal(err)
+	logger.Error(err.Error())
+	// terminate the application with exit code 1.
+	os.Exit(1)
 }
